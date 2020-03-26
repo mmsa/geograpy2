0fcc533eda827fbf93c2b53d047e9d8b41a6587a
Mohamed Mostafa <mmsa12@gmail.com>
2020-03-26 15:03:02 +0000

    Update setup.py
    Update the library to -> newspaper3k

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='geograpy2',
       install_requires=[
             'numpy',
             'nltk',
-            'newspaper',
+            'newspaper3k',
             'jellyfish',
             'pycountry'
       ],
